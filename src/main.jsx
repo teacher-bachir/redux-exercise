94575fd6360628151a7c154124c77294cc86b327
programming-teacher <adi.programming.data@gmail.com>
2025-09-02 22:31:28 +0300

    feat: wrap App component with Redux Provider for state management

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './features/app/App.jsx'
+import { Provider } from 'react-redux'
+import store from './features/app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
 )
