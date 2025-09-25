8f7382075231f82495234a0204c59c5e8d1c64cf
programming-teacher <adi.programming.data@gmail.com>
2025-09-25 22:35:16 +0300

    feat: implement Navbar component with user greeting and navigation links

---
+++ src/features/app/Navbar.jsx
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux"
+
+export default function Navbar() {
+    const { currentUser } = useSelector(state => state.users);
+
+    return (
+        <div>
+            <nav>
+                <h1>Welcome {currentUser?.username ?? 'Guest'}</h1>
+                <ul>
+                    {/* TODO: replace <a> tags with <NavLink> components */}
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">recipes</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
