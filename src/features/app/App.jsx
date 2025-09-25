7070ffcfbc01105563c4d294b596846a1b2453aa
programming-teacher <adi.programming.data@gmail.com>
2025-09-26 00:04:59 +0300

    feat: create SettingsExamplePage component with Button, Image, and List

--- src/features/app/App.jsx
+++ src/features/app/App.jsx
@@ -1,10 +1,8 @@
 import Settings from '../settings/Settings'
+import SettingsExamplePage from '../settings/SettingsExamplePage'
 import Login from '../users/Login'
 import Register from '../users/Register'
 import './App.css'
-import Button from './Button'
-import Image from './Image'
-import List from './List'
 import Navbar from './Navbar'
 
 function App() {
@@ -16,11 +14,7 @@ function App() {
                 <Register />
             </div>
             <Settings />
-            <Button hebrew='לחץ עלי!' english='Click Me!' />
-            <Image url={'https://www.z-zol.co.il/images/itempics/59030_large.jpg'} hebrew='כדור' english='Ball' />
-            <List
-                hebrew={['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת']}
-                english={['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']} />
+            <SettingsExamplePage />
         </>
     )
 }
---
+++ src/features/settings/SettingsExamplePage.jsx
@@ -0,0 +1,14 @@
+import Button from "../app/Button";
+import Image from "../app/Image";
+import List from "../app/List";
+
+export default function SettingsExamplePage() {
+    return (<>
+        <div>Settings Example Page</div>
+        <Button hebrew='לחץ עלי!' english='Click Me!' />
+        <Image url={'https://www.z-zol.co.il/images/itempics/59030_large.jpg'} hebrew='כדור' english='Ball' />
+        <List
+            hebrew={['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת']}
+            english={['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']} />
+    </>)
+}
