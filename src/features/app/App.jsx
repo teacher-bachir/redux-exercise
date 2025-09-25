8183b07bbf0e96d9ff902cb43b8863732963b519
programming-teacher <adi.programming.data@gmail.com>
2025-09-25 23:05:55 +0300

    feat: enhance Navbar component with language-based greeting and styling

--- src/features/app/App.jsx
+++ src/features/app/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 import Button from './Button'
 import Image from './Image'
 import List from './List'
+import Navbar from './Navbar'
 
 function App() {
     return (
         <>
+            <Navbar />
             <Settings />
             <Button hebrew='לחץ עלי!' english='Click Me!' />
             <Image url={'https://www.z-zol.co.il/images/itempics/59030_large.jpg'} hebrew='כדור' english='Ball' />
--- src/features/app/Navbar.jsx
+++ src/features/app/Navbar.jsx
@@ -1,18 +1,25 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import './Navbar.css';
 
 export default function Navbar() {
+    const { language } = useSelector((state) => state.settings);
     const { currentUser } = useSelector(state => state.users);
 
+    const direction = (language === 'he') ? 'rtl' : 'ltr';
+
     return (
-        <div>
+        <div className="Navbar" style={{ direction }}>
             <nav>
-                <h1>Welcome {currentUser?.name ?? 'Guest'}</h1>
+                {language === 'en' ?
+                    (<h1>Welcome {currentUser?.name ?? 'Guest'}</h1>) :
+                    (<h1>שלום {currentUser?.name ?? 'אורח'}</h1>)}
                 <ul>
                     {/* TODO: replace <a> tags with <NavLink> components */}
                     <li><a href="#">home</a></li>
                     <li><a href="#">about</a></li>
                     <li><a href="#">recipes</a></li>
                 </ul>
+                
             </nav>
         </div>
     )
