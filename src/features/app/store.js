c5ebe7b8a15e9653f3ff301f956c52e236cc2998
programming-teacher <adi.programming.data@gmail.com>
2025-09-26 04:17:39 +0300

    feat: add recipes reducer to the store configuration

--- src/features/app/store.js
+++ src/features/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import settingsReducer from "../settings/settingsSlice";
 import usersReducer from "../users/usersSlice";
+import recipesReducer from "../recipes/recipesSlice";
 
 const store = configureStore({
     reducer: {
         settings: settingsReducer,
         users: usersReducer,
+        recipes: recipesReducer,
     },
 });
 
