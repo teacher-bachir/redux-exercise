17d1c4df362df3d45dfa26fff8dc18619522bc39
programming-teacher <adi.programming.data@gmail.com>
2025-09-26 07:19:20 +0300

    refactor: reorganize Navbar component structure for improved readability

--- src/features/app/Navbar.jsx
+++ src/features/app/Navbar.jsx
@@ -3,32 +3,32 @@ import './Navbar.css';
 import { logout } from "../users/usersSlice";
 import { NavLink } from "react-router-dom";
 
-export default function Navbar() {
-    const STRINGS = {
-        en: {
-            welcome: 'Welcome',
-            guest: 'Guest',
-            logout: 'Logout',
-            home: 'Home',
-            settings: 'Settings',
-            settingsExample: 'Settings Example',
-            login: 'Login',
-            register: 'Register',
-            recipes: 'Recipes'
-        },
-        he: {
-            welcome: 'ברוך הבא',
-            guest: 'אורח',
-            logout: 'התנתקות',
-            home: 'בית',
-            settings: 'הגדרות',
-            settingsExample: 'דוגמא להגדרות',
-            login: 'התחברות',
-            register: 'הרשמה',
-            recipes: 'מתכונים'
-        }
-    };
+const STRINGS = {
+    en: {
+        welcome: 'Welcome',
+        guest: 'Guest',
+        logout: 'Logout',
+        home: 'Home',
+        settings: 'Settings',
+        settingsExample: 'Settings Example',
+        login: 'Login',
+        register: 'Register',
+        recipes: 'Recipes'
+    },
+    he: {
+        welcome: 'ברוך הבא',
+        guest: 'אורח',
+        logout: 'התנתקות',
+        home: 'בית',
+        settings: 'הגדרות',
+        settingsExample: 'דוגמא להגדרות',
+        login: 'התחברות',
+        register: 'הרשמה',
+        recipes: 'מתכונים'
+    }
+};
 
+export default function Navbar() {
     const { language } = useSelector((state) => state.settings);
     const { currentUser } = useSelector(state => state.users);
 
@@ -41,12 +41,12 @@ export default function Navbar() {
             <nav>
                 <h1>{STRINGS[language].welcome} {currentUser?.name ?? STRINGS[language].guest}</h1>
                 <ul>
-                    <NavLink to="">{STRINGS[language].home}</NavLink>
-                    <NavLink to="settings">{STRINGS[language].settings}</NavLink>
-                    <NavLink to="settings/example">{STRINGS[language].settingsExample}</NavLink>
-                    <NavLink to="login">{STRINGS[language].login}</NavLink>
-                    <NavLink to="register">{STRINGS[language].register}</NavLink>
-                    <NavLink to="recipes">{STRINGS[language].recipes}</NavLink>
+                    <li> <NavLink to="">{STRINGS[language].home}</NavLink></li>
+                    <li> <NavLink to="settings">{STRINGS[language].settings}</NavLink></li>
+                    <li> <NavLink to="settings/example">{STRINGS[language].settingsExample}</NavLink></li>
+                    <li> <NavLink to="login">{STRINGS[language].login}</NavLink></li>
+                    <li> <NavLink to="register">{STRINGS[language].register}</NavLink></li>
+                    <li> <NavLink to="recipes">{STRINGS[language].recipes}</NavLink></li>
                 </ul>
                 {
                     currentUser &&
