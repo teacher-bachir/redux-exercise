5ef9d8e545a8c42ea6451a9af01e1d962717369f
programming-teacher <adi.programming.data@gmail.com>
2025-09-26 14:48:04 +0300

    feat: add request and response interceptors to axios instance for enhanced error handling and request logging

--- src/features/app/axiosConfig.js
+++ src/features/app/axiosConfig.js
@@ -4,4 +4,31 @@ const instance = axios.create({
     baseURL: 'https://dummyjson.com'
 });
 
+instance.interceptors.request.use(
+    config => {
+        const limit = Math.floor(Math.random() * 10) + 10
+        config.url += `?limit=${limit}`;
+
+        console.log('Request made with ', config);
+        return config;
+    }
+);
+
+instance.interceptors.response.use(
+    response => response,
+    error => {
+        // Displaying the dialog for the error
+        const dialog = document.createElement('dialog');
+        dialog.textContent = error.message || 'Something went wrong!';
+        document.body.appendChild(dialog);
+        // Provide a way to close the dialog after a few seconds or on click
+        setTimeout(() => {
+            dialog.close(); dialog.remove();
+        }, 5000);
+        dialog.addEventListener('click', () => { dialog.close(); dialog.remove(); });
+        dialog.showModal()
+        return Promise.reject(error);
+    }
+);
+
 export default instance;
