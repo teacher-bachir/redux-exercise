880736ff1596352d5f71425f9403149c3d5e1277
programming-teacher <adi.programming.data@gmail.com>
2025-09-26 03:29:09 +0300

    fix: change Router from BrowserRouter to HashRouter for better routing support

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './features/app/App.jsx'
 import { Provider } from 'react-redux'
 import store from './features/app/store.js'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
